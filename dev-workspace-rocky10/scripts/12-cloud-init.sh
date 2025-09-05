80b33bbd18b6ed0ea53bc9249ad1460a246264b4
James Cahill <jcahill@ieee.org>
2025-09-05 18:17:25 +0100

    (dev-workspace-rocky10): Reset cloud-init before final in-build shutdown

--- dev-workspace-rocky10/scripts/12-cloud-init.sh
+++ dev-workspace-rocky10/scripts/12-cloud-init.sh
@@ -7,3 +7,9 @@ systemctl enable cloud-init-local
 systemctl enable cloud-init
 systemctl enable cloud-config
 systemctl enable cloud-final
+
+# Remove cloud-init artifacts to simulate clean instance
+# Should re-run cloud-init on subsequent boot.
+# Ref: https://cloudinit.readthedocs.io/en/latest/reference/cli.html
+# https://cloudinit.readthedocs.io/en/latest/howto/rerun_cloud_init.html
+cloud-init clean --logs --configs=all --seed
